1e40d06c4874d796bff76d0772b22b46d1215572
luiz213 <luiz.oliveira@simples.software>
2024-06-04 13:41:53 -0300

    Last Changes

--- src/main/java/com/employeemanagement/employeemanagement/dto/TrainingDTO.java
+++ src/main/java/com/employeemanagement/employeemanagement/dto/TrainingDTO.java
@@ -8,7 +8,7 @@ public class TrainingDTO {
 
 	private Long id;
 
-	private String name;
+	private String title;
 
 	private String description;
 
@@ -20,12 +20,12 @@ public class TrainingDTO {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getDescription() {
---
+++ src/main/java/com/employeemanagement/employeemanagement/entity/EmployeeTrainingKey.java
@@ -0,0 +1,5 @@
+package com.employeemanagement.employeemanagement.entity;
+
+public class EmployeeTrainingKey {
+
+}
