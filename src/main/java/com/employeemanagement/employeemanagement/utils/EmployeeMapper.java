64aaf2f8d7cf314faebd285816c5a2deafd27e3c
luiz213 <luiz.oliveira@simples.software>
2024-06-06 14:26:42 -0300

    fix insert employee

--- src/main/java/com/employeemanagement/employeemanagement/utils/EmployeeMapper.java
+++ src/main/java/com/employeemanagement/employeemanagement/utils/EmployeeMapper.java
@@ -1,6 +1,5 @@
 package com.employeemanagement.employeemanagement.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.employeemanagement.employeemanagement.dto.EmployeeDTO;
 import com.employeemanagement.employeemanagement.entity.Category;
@@ -8,9 +7,6 @@ import com.employeemanagement.employeemanagement.entity.Employee;
 
 @Component
 public class EmployeeMapper {
-	
-	@Autowired
-	private CategoryMapper categoryMapper;
 
 	public Employee covertToEntity(EmployeeDTO dto) {
 		Employee entity = new Employee();
@@ -19,13 +15,9 @@ public class EmployeeMapper {
 		entity.setMiddleName(dto.getMiddleName());
 		entity.setLastName(dto.getLastName());
 		entity.setCpf(dto.getCpf());
-		Category category = getCategoryMapper().covertToEntity(dto.getCategory());
+		Category category = new Category(dto.getCategoryId());
 		entity.setCategory(category);
 		return entity;
 	}
 	
-	private CategoryMapper getCategoryMapper() {
-		return categoryMapper;
-	}
-
 }
