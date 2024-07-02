393030adce351b057840791c19893bef3a8ab0d0
PietroSgarbosa <pietrosgarbosa@gmail.com>
2024-07-02 11:37:55 -0300

    Testando configuração do cors

--- src/main/java/com/employeemanagement/employeemanagement/config/SecurityConfig.java
+++ src/main/java/com/employeemanagement/employeemanagement/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.employeemanagement.employeemanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,37 +11,41 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import java.util.Arrays;
 
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 	
-//    @SuppressWarnings({ "deprecation" })
 //	@Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http
-//                .cors(withDefaults())
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeRequests(requests -> requests
-//                        .anyRequest().authenticated());
+//            .cors(Customizer.withDefaults())
+//            .csrf().disable()
+//            .authorizeHttpRequests(auth -> auth
+//            		.requestMatchers("/h2-console/**").permitAll()
+//            		.anyRequest().authenticated())
+//            .formLogin(formLogin ->
+//            formLogin.loginPage("/login") 
+//                     .permitAll() 
+//            );
 //        return http.build();
 //    }
-//    
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); //Permitindo métodos do protocolo HTTP
-//        configuration.setAllowedHeaders(Arrays.asList("*")); //Permitindo todos os headers
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        //configuration.setAllowedMethods(Arrays.asList("*")); //Permitindo métodos do protocolo HTTP
+        //configuration.setAllowedHeaders(Arrays.asList("*")); //Permitindo todos os headers
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
